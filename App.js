3d03a73a69295b1196df8be42cd615b246ed6a68
AppDev8890 <hannsg889@gmail.com>
2023-05-08 13:56:25 +0500

    workijng chatbot added

--- App.js
+++ App.js
@@ -14,6 +14,8 @@ import ProfileChatbot from './components/ProfileChatbot';
 import Testing from './components/Testing';
 import ConfirmProfile from './components/ConfirmProfile';
 import SurahFateha from './components/surahs/Fateha/SurahFateha';
+import Second from './components/Second';
+import SurahBaqrah from './components/surahs/Baqrah/SurahBaqrah';
 
 
 const Stack = createNativeStackNavigator();
@@ -27,6 +29,7 @@ const App = () => {
          <Stack.Screen name="AllSurah" component={AllSurah} options={{headerShown: false}} />
          <Stack.Screen name="English" component={English}  />
          <Stack.Screen name="Other" component={Other}  />
+         <Stack.Screen name="Second" component={Second}  />
          <Stack.Screen name="GetUserInfo" component={GetUserInfo}  />
          <Stack.Screen name="Testing" component={Testing}  />
          {/*Screen thats get user information before moving to chatbot*/}
@@ -34,6 +37,7 @@ const App = () => {
          <Stack.Screen name="ConfirmProfile" component={ConfirmProfile}  />
          <Stack.Screen name="Chatbot" component={Chatbot}  />
          <Stack.Screen name="SurahFateha" component={SurahFateha}  />
+         <Stack.Screen name="SurahBaqrah" component={SurahBaqrah}  />
          <Stack.Screen name="ArabicMain" component={ArabicMain} options={{headerShown: false}} />
 
         </Stack.Navigator>
--- components/Chatbot.js
+++ components/Chatbot.js
@@ -1,17 +1,17 @@
-import { View, Text, TouchableOpacity,StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity,StyleSheet,FlatList, TextInput } from 'react-native'
 import React,{useState} from 'react'
 import axios from 'axios';
-import { FlatList, TextInput } from 'react-native-gesture-handler';
 const Chatbot = () => {
     const [data,setData]=useState([]);
 
-    const apiKey='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';
+    const apiKey='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';
     const apiUrl="https://api.openai.com/v1/engines/text-davinci-002/completions";
     const [textInput,setTextInput]=useState('');
 
     const handleSend=async()=>{
       try{
         const prompt=textInput;
+        alert(textInput);
         const response=await axios.post(apiUrl,{
             prompt:prompt,
             max_tokens:1024,
@@ -23,8 +23,10 @@ const Chatbot = () => {
             }
         });
         const text=response.data.choices[0].text;
-        setData([...data,{type:'user','text':textInput},{type:'bot','text':text}]);
-        setTextInput('');
+        alert("text",text);
+        setData([...data,{type:'user','text':textInput}]);
+        // setTextInput('');
+        alert("Data",data);
       }
       catch(err){
         alert(err)
@@ -39,7 +41,7 @@ const Chatbot = () => {
         style={styles.body}
         renderItem={(item)=>(
             <View style={{flexDirection:'row',padding:10}}>
-                <Text style={{fontWeight:'bold',color:item.type==='user'?'green':'red'}}>
+                <Text style={{fontWeight:'bold',color:item.type==='user'?'green':'black'}}>
                     {item.type==='user'?'Ninza':'Bot'}
                 </Text>
                 <Text style={styles.bot}>{item.text}</Text>
